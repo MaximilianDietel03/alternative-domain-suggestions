c4c1bd75583078350a70a4209a0a5fff8314e41c
Maximilian Dietel <77168125+MaximilianDietel03@users.noreply.github.com>
2022-12-29 15:58:48 +0100

    MVP

---
+++ pages/api/generate.ts
@@ -0,0 +1,35 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Suggest alternatives to the following domain and keep the same TLD.
+Domain:
+`;
+const generateAction = async (req: any, res: any) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  let baseCompletion;
+  try {
+    baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${req.body.userInput}`,
+      temperature: 0.7,
+      max_tokens: 250,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+    return;
+  }
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
